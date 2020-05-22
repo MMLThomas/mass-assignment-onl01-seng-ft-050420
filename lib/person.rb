a398b1559e3487584b3b85775996be761ac239ee
michael thomas <michael.thomas2014@gmail.com>
2020-05-22 15:51:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_leng, :pant_width
   
   
-  def initiliztion(attributes)
+  def initializtion(attributes)
     attributes.each{|key, value| self.send(("#{key}="), value)}
   end
     
